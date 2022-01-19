30a97fb7ab1418f0f91535bd94d39b6d9a46c815
Hussain <nisyh3327@wrdsb.ca>
2022-01-19 13:01:18 -0500

    I finished making the card picker and started making a player/dealer and a card limit.

--- A4BlackJack.java
+++ A4BlackJack.java
@@ -3,23 +3,28 @@ import java.util.Scanner;
 
 public class A4BlackJack {
     
-    static int[] Ace = {1,11}; 
+    static int Ace = 1; //make an if statement for picking either 1 pr 11 when calculating the sum of the two cards.
     static int King = 10; 
     static int Queen = 10; 
     static int Jack = 10; 
     
-    
-   /* public int card(int cardRank) {
-        String[] cardNum = {"Unused", Ace, "2", "3", "4", "5", "6", "7", "8", "9", "10", Jack, Queen, King};
-        int randomCard = randomNumGenerator.nextInt(13) + 1;
-        System.out.println(cardNum[randomCard]);
-    } */
+    // picks a card from Ace to King
+    public static int card() { 
+        Random rand = new Random(); 
+        int cardNum[] = {Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King};
+        int test [][] = {cardNum, cardNum, cardNum, cardNum}; 
+        int randSuit = rand.nextInt(4); 
+        int randomCard = rand.nextInt(13);
+        int card = test[randSuit][randomCard];
+        System.out.println(randSuit + " " + randomCard); 
+        return test[randSuit][randomCard]; 
+        
+    } 
 
-    public static void main(String[] args) {
-        Random randomNumGenerator = new Random();
-        int[] cardNum = {0 , Ace[], 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King};
-        int randomCard = randomNumGenerator.nextInt(13) + 1;
-        System.out.println(cardNum[randomCard]);
+    
+
+    public static void main(String[] args) { 
+        System.out.println(card()); 
     }
 
 }
